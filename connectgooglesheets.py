88ffc57ce9ae75d57dab9e62a5fd58f89099b120
terriblebassist <asingh175@gmail.com>
2021-03-29 05:28:42 +0530

    change oauth

--- connectgooglesheets.py
+++ connectgooglesheets.py
@@ -6,26 +6,31 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from decouple import config
+from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 
 def gsheet_api_check(SCOPES):
     creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            secret = json.loads(config('GOOGLE_CREDENTIALS_JSON'))
-            flow = InstalledAppFlow.from_client_config(secret,SCOPES)
-            # .from_client_secrets_file(
-            #     'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    if config('GOOGLE_CREDENTIALS'):
+        creds = service_account.Credentials.from_service_account_file(
+        "google-credentials.json", scopes=SCOPES)
+    else:
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                secret = json.loads(config('GOOGLE_CREDENTIALS_JSON'))
+                flow = InstalledAppFlow.from_client_config(secret,SCOPES)
+                # .from_client_secrets_file(
+                #     'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
     return creds
 
 
